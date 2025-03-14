fc6728f22fa17868f96aaec5a6cb69de280ab9ff
MuFaqihi <mmnouc2@gmail.com>
2025-03-15 02:13:45 +0300

    Update xo.c

--- xo.c
+++ xo.c
@@ -1,20 +1,52 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-char board[3][3];  // لوحة اللعب
-char player = 'X';  // اللاعب الأول يبدأ بـ X
-int row, col;  // لتحديد الصف والعمود الذي سيتم اللعب فيه
-
-// دالة لتهيئة اللوحة
-void initializeBoard() {
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            board[i][j] = ' ';
-        }
+void print_winner_symbol(char symbol) {
+    if (symbol == 'X') {
+        printf("\n");
+        printf("X   X\n");
+        printf(" X X \n");
+        printf("  X  \n");
+        printf(" X X \n");
+        printf("X   X\n");
+    } else if (symbol == 'O') {
+        printf("\n");
+        printf("  OOO  \n");
+        printf(" O   O \n");
+        printf(" O   O \n");
+        printf(" O   O \n");
+        printf("  OOO  \n");
     }
+    printf("\n");
 }
 
-// دالة لعرض اللوحة
-void printBoard() {
+void display_logo() {
+    printf("\n");
+    printf("  _    _       _     _             \n");
+    printf(" | |  | |     | |   (_)            \n");
+    printf(" | |__| |_   _| |__  _ _ __   __ _ \n");
+    printf(" |  __  | | | | '_ \\| | '_ \\ / _` |\n");
+    printf(" | |  | | |_| | | | | | | | | (_| |\n");
+    printf(" |_|  |_|\__,_|_| |_|_|_| |_|\__, |\n");
+    printf("                               __/ |\n");
+    printf("                              |___/ \n");
+    printf("\nWelcome to Holberton & Tuwaiq Tic-Tac-Toe! or XO\n\n");
+    printf("  _______                   _     \n");
+    printf(" |__   __|                 (_)    \n");
+    printf("    | | ___ _ __ _ __  _   _ _ ___\n");
+    printf("    | |/ _ \\ '__| '_ \\| | | | / __|\n");
+    printf("    | |  __/ |  | | | | |_| | \\__ \\\n");
+    printf("    |_|\___|_|  |_| |_|\__,_|_|___/\n");
+    printf("\n\n");
+}
+
+typedef struct {
+    char name[50];
+    char symbol; // X أو O
+} Player;
+
+void display_board(char board[3][3]) {
     printf("\n");
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
@@ -22,97 +54,86 @@ void printBoard() {
             if (j < 2) printf("|");
         }
         printf("\n");
-        if (i < 2) printf("---|---|---\n");
+        if (i < 2) printf("---+---+---\n");
     }
     printf("\n");
 }
 
-// دالة للتحقق من الفائز
-int checkWin() {
-    // التحقق من الصفوف
+bool check_winner(char board[3][3], char symbol) {
     for (int i = 0; i < 3; i++) {
-        if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ' ') {
-            return 1;
-        }
+        if (board[i][0] == symbol && board[i][1] == symbol && board[i][2] == symbol)
+            return true;
+        if (board[0][i] == symbol && board[1][i] == symbol && board[2][i] == symbol)
+            return true;
     }
-
-    // التحقق من الأعمدة
-    for (int j = 0; j < 3; j++) {
-        if (board[0][j] == board[1][j] && board[1][j] == board[2][j] && board[0][j] != ' ') {
-            return 1;
-        }
-    }
-
-    // التحقق من القطر الأول
-    if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ') {
-        return 1;
-    }
-
-    // التحقق من القطر الثاني
-    if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != ' ') {
-        return 1;
-    }
-
-    return 0;  // لا يوجد فائز بعد
+    if (board[0][0] == symbol && board[1][1] == symbol && board[2][2] == symbol)
+        return true;
+    if (board[0][2] == symbol && board[1][1] == symbol && board[2][0] == symbol)
+        return true;
+    return false;
 }
 
-// دالة للتحقق من أن اللعبة انتهت بالتعادل
-int checkDraw() {
+bool is_full(char board[3][3]) {
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
-            if (board[i][j] == ' ') {
-                return 0;  // إذا كان هناك مكان فارغ، فلا يوجد تعادل بعد
+            if (board[i][j] >= '1' && board[i][j] <= '9')
+                return false;
+        }
+    }
+    return true;
+}
+
+int main(int argc, char *argv[]) {
+    display_logo();
+    
+    char board[3][3] = {{'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}};
+    Player players[2];
+    int turn = 0;
+    int choice;
+    bool winner_found = false;
+
+    if (argc > 2) {
+        snprintf(players[0].name, 50, "%s", argv[1]);
+        snprintf(players[1].name, 50, "%s", argv[2]);
+    } else {
+        snprintf(players[0].name, 50, "Player 1");
+        snprintf(players[1].name, 50, "Player 2");
+    }
+
+    players[0].symbol = 'X';
+    players[1].symbol = 'O';
+
+    while (!winner_found && !is_full(board)) {
+        display_board(board);
+        printf("%s's turn (%c). Enter a number (1-9): ", players[turn].name, players[turn].symbol);
+        
+        if (scanf("%d", &choice) != 1 || choice < 1 || choice > 9) {
+            printf("Invalid input. Please enter a number between 1 and 9.\n");
+            while (getchar() != '\n');
+            continue;
+        }
+        
+        int row = (choice - 1) / 3;
+        int col = (choice - 1) % 3;
+        
+        if (board[row][col] >= '1' && board[row][col] <= '9') {
+            board[row][col] = players[turn].symbol;
+            if (check_winner(board, players[turn].symbol)) {
+                display_board(board);
+                printf("%s wins!\n", players[turn].name);
+                print_winner_symbol(players[turn].symbol);
+                winner_found = true;
+            } else {
+                turn = 1 - turn;
             }
-        }
-    }
-    return 1;  // إذا كانت كل الخلايا ممتلئة والتعادل
-}
-
-// دالة لتنفيذ حركة اللاعب
-void playerMove() {
-    printf("اللاعب %c، اختر الصف (0-2) والعمود (0-2): ", player);
-    scanf("%d %d", &row, &col);
-
-    if (row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == ' ') {
-        board[row][col] = player;
-    } else {
-        printf("موقع غير صالح! حاول مرة أخرى.\n");
-        playerMove();
-    }
-}
-
-// دالة لتبديل اللاعبين
-void switchPlayer() {
-    if (player == 'X') {
-        player = 'O';
-    } else {
-        player = 'X';
-    }
-}
-
-int main() {
-    int gameStatus = 0;  // حالة اللعبة: 0 = اللعبة مستمرة، 1 = فوز، 2 = تعادل
-    initializeBoard();  // تهيئة اللوحة
-
-    while (gameStatus == 0) {
-        printBoard();  // عرض اللوحة
-        playerMove();  // إجراء حركة اللاعب
-        gameStatus = checkWin();  // التحقق من الفوز
-        if (gameStatus == 0) {
-            gameStatus = checkDraw();  // التحقق من التعادل
-        }
-
-        if (gameStatus == 0) {
-            switchPlayer();  // تبديل اللاعبين
+        } else {
+            printf("Invalid move. The cell is already occupied.\n");
         }
     }
 
-    printBoard();  // عرض اللوحة الأخيرة
-
-    if (gameStatus == 1) {
-        printf("اللاعب %c فاز!\n", player);  // إعلان الفائز
-    } else {
-        printf("اللعبة انتهت بالتعادل!\n");  // إذا كان التعادل
+    if (!winner_found) {
+        display_board(board);
+        printf("It's a draw!\n");
     }
 
     return 0;
